4caa9c9669451fb7f5fe68037c5a80c86a727a09
David Heinemeier Hansson <david@loudthinking.com>
2021-09-18 21:06:04 +0200

    Digested requests will never hit the server
    They're dealt with directly from puma (or nginx/whatever)

--- lib/propshaft/server.rb
+++ lib/propshaft/server.rb
@@ -6,38 +6,24 @@ class Propshaft::Server
   end
 
   def call(env)
-    if asset = @load_path.find(requested_asset(env))
-      [ 200, headers(asset), [ asset.content ] ]
+    if asset = @load_path.find(requested_path(env))
+      [ 200, headers(asset, env), [ asset.content ] ]
     else
       [ 404, { "Content-Type" => "text/plain", "Content-Length" => "9" }, [ "Not found" ] ]
     end
   end
 
   private
-    def requested_asset(env)
-      remove_digest(requested_path(env))
-    end
-
     def requested_path(env)
       Rack::Utils.unescape(env["PATH_INFO"].to_s.sub(/^\//, ""))
     end
 
-    def remove_digest(path)
-      if digest = extract_digest(path)
-        path.sub("-#{digest}", "")
-      else
-        path
-      end
-    end
-
-    def extract_digest(path)
-      path[/-([0-9a-f]{7,128})\.[^.]+\z/, 1]
-    end
-
-    def headers(asset)
+    def headers(asset, env)
       {
         "Content-Length" => asset.length.to_s,
-        "Content-Type" => asset.content_type
+        "Content-Type"   => asset.content_type,
+        "ETag"           => asset.digest,
+        "Cache-Control"  => "public, must-revalidate"
       }
     end
 end
