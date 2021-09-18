b1da9ea68a773eebe83bba88c49cd5d7d5fe2573
David Heinemeier Hansson <david@loudthinking.com>
2021-09-18 16:40:56 +0200

    Proper railtie

--- lib/propshaft/railtie.rb
+++ lib/propshaft/railtie.rb
@@ -1,8 +1,23 @@
+require "rails"
+require "rails/railtie"
+require "active_support/ordered_options"
+
+require "propshaft/load_path"
+require "propshaft/server"
+
 module Propshaft
   class Railtie < ::Rails::Railtie
-    config.assets = OrderedOptions.new
+    config.assets = ActiveSupport::OrderedOptions.new
     config.assets.paths    = []
+    config.assets.precompile = [] # Compatibility shim
     config.assets.prefix   = "/assets"
     config.assets.manifest = nil
+
+    config.after_initialize do |app|
+      app.assets = Propshaft::LoadPath.new(app.config.assets.paths)
+      app.routes.prepend do
+        mount Propshaft::Server.new(app.assets) => app.config.assets.prefix
+      end
+    end
   end
 end
