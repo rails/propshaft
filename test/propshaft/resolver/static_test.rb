c93c4f0ae5501ef467bb7ad39d02470c4011e478
David Heinemeier Hansson <david@loudthinking.com>
2021-09-19 11:14:55 +0200

    Test static resolver

--- test/propshaft/resolver/static_test.rb
+++ test/propshaft/resolver/static_test.rb
@@ -3,12 +3,16 @@ require "propshaft/resolver/static"
 
 class Propshaft::Resolver::StaticTest < ActiveSupport::TestCase
   setup do
-    @load_path = Propshaft::LoadPath.new Pathname.new("#{__dir__}/../../fixtures/assets/first_path")
-    @resolver  = Propshaft::Resolver::Dynamic.new(load_path: @load_path, prefix: "/assets")
+    @resolver = Propshaft::Resolver::Static.new(
+      manifest_path: Pathname.new("#{__dir__}/../../fixtures/output/manifest.json"),
+      prefix: "/assets"
+    )
   end
 
   test "resolving present asset returns uri path" do
-    assert_equal "/assets/one.txt", @resolver.resolve("one.txt")
+    assert_equal \
+      "/assets/one-f2e1ec14d6856e1958083094170ca6119c529a73.txt",
+      @resolver.resolve("one.txt")
   end
 
   test "resolving missing asset returns nil" do
