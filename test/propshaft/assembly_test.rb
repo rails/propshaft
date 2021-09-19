573db1580709a44b3357f1055adf2bed1d5c40d5
David Heinemeier Hansson <david@loudthinking.com>
2021-09-19 11:35:54 +0200

    Test assembly resolver picking

---
+++ test/propshaft/assembly_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+require "propshaft/assembly"
+require "active_support/ordered_options"
+
+class Propshaft::AssetTest < ActiveSupport::TestCase
+  test "uses static resolver when manifest is present" do
+    assembly = Propshaft::Assembly.new(ActiveSupport::OrderedOptions.new.tap { |config| 
+      config.manifest_path = Pathname.new("#{__dir__}/../fixtures/output/manifest.json")
+      config.prefix = "/assets"
+    })
+
+    assert assembly.resolver.is_a?(Propshaft::Resolver::Static)
+  end
+
+  test "uses dynamic resolver when manifest is missing" do
+    assembly = Propshaft::Assembly.new(ActiveSupport::OrderedOptions.new.tap { |config| 
+      config.manifest_path = Pathname.new("#{__dir__}/../fixtures/output/not-a-manifest.json")
+      config.prefix = "/assets"
+    })
+
+    assert assembly.resolver.is_a?(Propshaft::Resolver::Dynamic)
+  end
+end
