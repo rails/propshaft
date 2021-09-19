517150878173f6a6af25e6129b2d6009abd5db35
David Heinemeier Hansson <david@loudthinking.com>
2021-09-19 11:04:59 +0200

    Fix variables

--- lib/propshaft/resolver/dynamic.rb
+++ lib/propshaft/resolver/dynamic.rb
@@ -1,13 +1,13 @@
 module Propshaft::Resolver
   class Dynamic
-    attr_reader :path, :prefix
+    attr_reader :load_path, :prefix
 
     def initialize(load_path:, prefix:)
-      @load_path, @prefix = prefix, load_path
+      @load_path, @prefix = load_path, prefix
     end
 
     def resolve(logical_path)
-      if asset = load_path.find(path)
+      if asset = load_path.find(logical_path)
         File.join prefix, asset.logical_path
       end
     end
