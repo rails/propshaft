6ad6b241df55178e7692fb3eab6f1e02869965ee
Richard Macklin <richard.github@nrm.com>
2021-12-04 11:20:47 -0800

    Add an integration test to ensure the gem works in a real application
    Without the changes from 0fd78144d70c987cf33da97b4b6471ca322b021e,
    the second test fails with:
    `ActionView::Template::Error expected but nothing was raised.`
    because the gem would simply generate broken tags like
    `<link rel="stylesheet" href="" />` and `<script src=""></script>`
    rather than raising an exception to loudly inform the developer that the
    given asset can't be resolved.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  get 'sample/load_real_assets'
+  get 'sample/load_nonexistent_assets'
 end
