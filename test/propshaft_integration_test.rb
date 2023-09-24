fe0712076be5b29c0d5bc12800b188b09e43fd8b
dhh <david@loudthinking.com>
2023-09-24 09:03:25 -0700

    Add helper to include all stylesheets in the load path

--- test/propshaft_integration_test.rb
+++ test/propshaft_integration_test.rb
@@ -3,8 +3,12 @@ require "test_helper"
 class PropshaftIntegrationTest < ActionDispatch::IntegrationTest
   test "should be able to resolve real assets" do
     get sample_load_real_assets_url
+
     assert_response :success
+
     assert_select 'link[href="/assets/hello_world-4137140a1298c3924d5f7135617c23e23fb167a8.css"]'
+    assert_select 'link[href="/assets/goodbye-b1dc9940e9800d8bc96f7434617c043e58277419.css"]'
+
     assert_select 'script[src="/assets/hello_world-888761f849ba63a95a56f6ef898a9eb70ca4c46e.js"]'
   end
 
