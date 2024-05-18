47951bb186f166de176b1c74ee0ca629ccdad531
David Heinemeier Hansson <david@hey.com>
2024-05-17 17:11:32 -0700

    Protect against missing references

--- lib/propshaft/compiler/css_asset_urls.rb
+++ lib/propshaft/compiler/css_asset_urls.rb
@@ -13,7 +13,7 @@ class Propshaft::Compiler::CssAssetUrls < Propshaft::Compiler
     asset.content.scan(ASSET_URL_PATTERN).each do |referenced_asset_url, _|
       referenced_asset = load_path.find(resolve_path(asset.logical_path.dirname, referenced_asset_url))
 
-      unless references.include?(referenced_asset)
+      if referenced_asset && references.exclude?(referenced_asset)
         references << referenced_asset
         references.merge referenced_by(referenced_asset, references: references)
       end
